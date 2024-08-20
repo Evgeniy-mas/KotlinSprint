eb9740eff1d54451590b66095ecab7982d6a09ee
Evgeniy <Evgrazhev@yandex.ru>
2024-08-20 12:56:15 +0300

    KS-1-2 create_variables_2

---
+++ src/main/kotlin/variables_lesson/Variable_lesson_3.kt
@@ -0,0 +1,24 @@
+package org.example.variables_lesson
+
+import jdk.jfr.Unsigned
+import org.jetbrains.annotations.Nullable
+
+fun main() {
+
+    val year = 1961
+
+    var hour  =  "09"
+    var minute = "07"
+
+   println("$year \n$hour \n$minute")
+
+    hour = "10"
+    minute = "55"
+    print("$hour:$minute")
+
+
+
+
+
+
+}
