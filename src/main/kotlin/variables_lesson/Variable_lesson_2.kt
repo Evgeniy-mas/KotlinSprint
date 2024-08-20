03e10fa51daf098f365e47178b8399beffd735d9
Evgeniy <Evgrazhev@yandex.ru>
2024-08-20 11:26:51 +0300

    KS-1-2 create_variables_2

---
+++ src/main/kotlin/variables_lesson/Variable_lesson_2.kt
@@ -0,0 +1,22 @@
+package org.example.variables_lesson
+
+fun main() {
+    val totalOrders:Int = 75
+    val thanksForOrder:String = "Спасибо за заказ в нашем магазине. Ждем вас снова."
+
+    println("Всего заказов $totalOrders")
+    println(thanksForOrder)
+
+    var workersShop = 2000
+   // println("Всего работников в магазине $workersShop.")
+
+    workersShop = 1999
+    println("Всего работников в магазине $workersShop.")
+
+
+
+
+
+
+
+}
