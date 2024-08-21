34962ae93920ff5ac6c2b184db3cfc30dd43de47
Evgeniy <Evgrazhev@yandex.ru>
2024-08-21 09:31:03 +0300

    KS-1-4 format space bar

--- src/main/kotlin/variables_lesson/KS-1-4 -create-variable.kt
+++ src/main/kotlin/variables_lesson/KS-1-4 -create-variable.kt
@@ -1,16 +1,11 @@
 package org.example.variables_lesson
 
-import jdk.jfr.Unsigned
-import org.jetbrains.annotations.Nullable
-
 fun main() {
-
     val year = 1961
-
     var hour  =  "09"
     var minute = "07"
 
-   println("$year \n$hour \n$minute")
+    println("$year \n$hour \n$minute")
 
     hour = "10"
     minute = "55"
