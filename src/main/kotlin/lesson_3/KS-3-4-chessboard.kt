5af7f41dc0cfe4dfa87f5ce004034913d1d5bf44
Evgeniy <Evgrazhev@yandex.ru>
2024-08-23 09:29:52 +0300

    KS-3-4-chessboard refactor sending.kt

--- src/main/kotlin/lesson_3/KS-3-4-chessboard.kt
+++ src/main/kotlin/lesson_3/KS-3-4-chessboard.kt
@@ -4,14 +4,14 @@ fun main() {
     var whereFrom = "E2"
     var where = "E4"
     var moveNumber = 1
-    var sending = whereFrom  + where  + moveNumber
+    var sending = "[$whereFrom-$where;$moveNumber]"
 
     println(sending)
 
     whereFrom = "D2"
     where = "D3"
     moveNumber = 2
-    sending = whereFrom  + where  + moveNumber
+    sending = "[$whereFrom-$where;$moveNumber]"
 
     println(sending)
 }
