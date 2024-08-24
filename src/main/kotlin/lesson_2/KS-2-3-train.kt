25c01b41ef5d9ac3620540ab2bbcb927a05bbb59
Evgeniy <Evgrazhev@yandex.ru>
2024-08-24 17:19:27 +0300

    KS-2-3-train refactor.kt

--- src/main/kotlin/lesson_2/KS-2-3-train.kt
+++ src/main/kotlin/lesson_2/KS-2-3-train.kt
@@ -4,12 +4,16 @@ fun main() {
     val hourStartTrain = 9
     val minuteStartTrain = 39
     val travelTimeMinute = 457
+    val constOneMinute = 60
 
-    val convertTimeStart = hourStartTrain * 60 + minuteStartTrain
+    val convertTimeStart = hourStartTrain * constOneMinute + minuteStartTrain
     val timeArriveInMinute = convertTimeStart + travelTimeMinute
 
-    val hourArrive = timeArriveInMinute / 60
-    val minuteArrive = timeArriveInMinute - hourArrive * 60
+    val hourArrive = timeArriveInMinute / constOneMinute
+    val minuteArrive = timeArriveInMinute % constOneMinute
 
-    println("Время прибытия поезда в: $hourArrive:$minuteArrive")
+    val hour = String.format("%02d", hourArrive)
+    val minute = String.format("%02d",  minuteArrive)
+
+    println("Время прибытия поезда в: $hour:$minute")
 }
