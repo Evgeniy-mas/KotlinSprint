f3d77d2d39e1cd6f49f69a69fba30c433d0ec5a1
Evgeniy <Evgrazhev@yandex.ru>
2024-08-24 16:05:06 +0300

    KS-5-3-lottery.kt

---
+++ src/main/kotlin/lesson_5/KS-5-3-lottery.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_5
+
+fun main() {
+    val firsNumber = 0..42
+    val secondNumber = 0..42
+
+    println("Угадайте два числа от 0 до 42")
+    println("Введите первое число:")
+
+    val first = readln().toInt()
+
+    println("Введите второе число:")
+
+    val second = readln().toInt()
+
+    if (first in firsNumber && second in secondNumber) {
+        println("Поздравляем! Вы выйграли главный приз!")
+    }
+    else if(first in firsNumber || second in secondNumber) {
+        println("Вы выйграли утешительный приз!")
+}
+    else {
+        println("Неудача!")
+    }
+
+    println("Для победы нужно было угадать числа:")
+    for (i in firsNumber) {
+        print("$i ")
+    }
+}
