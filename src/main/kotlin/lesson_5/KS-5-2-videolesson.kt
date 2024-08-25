e68feb507794e9792eebe466976ba52e53522a14
Evgeniy <Evgrazhev@yandex.ru>
2024-08-25 17:18:47 +0300

    KS-5-2-videolesson refactor add year.kt

--- src/main/kotlin/lesson_5/KS-5-2-videolesson.kt
+++ src/main/kotlin/lesson_5/KS-5-2-videolesson.kt
@@ -1,10 +1,15 @@
 package org.example.lesson_5
 
+import java.util.Calendar
+
 fun main() {
     println("Для входа введите год рождения:")
-
     val yearOfBirth = readln().toInt()
-    val userAge = 2024 - yearOfBirth
+
+    val calendar = Calendar.getInstance()
+    val yearNow = calendar.get(Calendar.YEAR)
+
+    val userAge = yearNow - yearOfBirth
 
     if (userAge >= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом!")
