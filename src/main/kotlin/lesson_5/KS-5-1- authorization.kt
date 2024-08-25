f3108bb37d7c1c1a9bdd0d2ce0c6236f709a0d9a
Evgeniy <Evgrazhev@yandex.ru>
2024-08-25 17:29:24 +0300

    KS-5-1-authorization refactor add variables.kt

--- src/main/kotlin/lesson_5/KS-5-1- authorization.kt
+++ src/main/kotlin/lesson_5/KS-5-1- authorization.kt
@@ -3,10 +3,12 @@ package org.example.lesson_5
 fun main() {
 println("Для входа сложите два числа: 12 и 11")
 
-    val answer = 23
+    val number1 = 12
+    val number2 = 11
+    val result = number1 + number2
 
     val read = readln().toInt()
-    if(read == answer) {
+    if(read == result) {
         println("Добро пожаловать!")
     }
     else {
