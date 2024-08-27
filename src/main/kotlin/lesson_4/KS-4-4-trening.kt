ed006a20f1013f259fed050880316eb7569c9422
Evgeniy <Evgrazhev@yandex.ru>
2024-08-27 09:07:20 +0300

    KS-4-4-trening refactor error.kt

--- src/main/kotlin/lesson_4/KS-4-4-trening.kt
+++ src/main/kotlin/lesson_4/KS-4-4-trening.kt
@@ -6,9 +6,9 @@ fun main() {
     val parityNumber = dayWorkout % 2
 
     println("Упражнения для рук: ${HAND_AND_PRESS == parityNumber}\n" +
-            "Упражнения для ног: ${LEGS_AND_PRESS == parityNumber}\n" +
-            "Упражнения для спины: ${HAND_AND_PRESS == parityNumber}\n" +
-            "Упражнения для пресса: ${LEGS_AND_PRESS == parityNumber}\n")
+            "Упражнения для ног: ${LEGS_AND_BACK == parityNumber}\n" +
+            "Упражнения для спины: ${LEGS_AND_BACK == parityNumber}\n" +
+            "Упражнения для пресса: ${HAND_AND_PRESS == parityNumber}\n")
 }
 const val HAND_AND_PRESS = 1
-const val LEGS_AND_PRESS = 0
+const val LEGS_AND_BACK = 0
