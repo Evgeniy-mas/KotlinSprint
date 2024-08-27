8e5a00457b5cb7dc0c7da2a3afb0aaf3cbbcba6d
Evgeniy <Evgrazhev@yandex.ru>
2024-08-27 08:57:34 +0300

    KS-4-4-trening.kt

---
+++ src/main/kotlin/lesson_4/KS-4-4-trening.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_4
+
+fun main() {
+    val dayWorkout = 5
+
+    val parityNumber = dayWorkout % 2
+
+    println("Упражнения для рук: ${HAND_AND_PRESS == parityNumber}\n" +
+            "Упражнения для ног: ${LEGS_AND_PRESS == parityNumber}\n" +
+            "Упражнения для спины: ${HAND_AND_PRESS == parityNumber}\n" +
+            "Упражнения для пресса: ${LEGS_AND_PRESS == parityNumber}\n")
+}
+const val HAND_AND_PRESS = 1
+const val LEGS_AND_PRESS = 0
