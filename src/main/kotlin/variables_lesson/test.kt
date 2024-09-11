c31faf7622e173ffe5ef4059cbf31323b77dc7bd
Evgeniy <Evgrazhev@yandex.ru>
2024-09-11 08:39:06 +0300

    KS-1

---
+++ src/main/kotlin/variables_lesson/test.kt
@@ -0,0 +1,42 @@
+package org.example.variables_lesson
+
+import java.util.StringTokenizer
+
+
+
+fun main() {
+
+val basket = arrayOf("Гвози", "Молоток", "Стэплер")
+
+    println("Для авторизаци введите логин и пароль:")
+    println("Введите логин:")
+
+    val Login = readln()?.isNullOrEmpty()
+
+    println("Введите пароль:")
+
+    val password = readln()?.isNullOrEmpty()
+
+
+
+}
+const val LOGIN = "user"
+const val PASSWORD = "1234"
+
+
+
+fun gener(i:String) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
