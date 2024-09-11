7f758424c300558895848afa5ec994ac1b9cf028
Evgeniy <Evgrazhev@yandex.ru>
2024-09-11 13:32:30 +0300

    KS-12-1-weather.kt

---
+++ src/main/kotlin/lesson_12/KS-12-1-weather.kt
@@ -0,0 +1,38 @@
+package org.example.lesson_12
+
+fun main() {
+    val sunday = Weather()
+
+    sunday.dayTimeTemp = "20"
+    sunday.dayNightTemp = "15"
+    sunday.dayRain = "Дождя нет"
+
+    sunday.getInfo()
+
+    sunday.seeInSky = "Луна"
+    sunday.number = 55
+
+    val monday = Weather()
+
+    monday.dayTimeTemp = "25"
+    monday.dayNightTemp = "17"
+    monday.dayRain = "Дождь днем"
+
+    monday.getInfo()
+
+    monday.seeInSky = "Облако"
+    monday.number = 100
+}
+
+class Weather {
+    lateinit var dayTimeTemp: String
+    lateinit var dayNightTemp: String
+    lateinit var dayRain: String
+
+    var seeInSky = "Солнце"
+    var number = 45
+
+    fun getInfo() {
+        println("Температура днем: $dayTimeTemp\nТемпература ночью: $dayNightTemp\nПогода: $dayRain")
+    }
+}
