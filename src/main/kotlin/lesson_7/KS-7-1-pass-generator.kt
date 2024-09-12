797ae98de5aa163323d6413c55796c2b36b21ecb
Evgeniy <Evgrazhev@yandex.ru>
2024-09-12 10:27:03 +0300

    KS-7-1-pass-generator-refactor.kt

--- src/main/kotlin/lesson_7/KS-7-1-pass-generator.kt
+++ src/main/kotlin/lesson_7/KS-7-1-pass-generator.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_7
 
 fun main() {
-    val char = ('a'..'z').toList()
-    val randomChar = (1..3).map { char.random() }.joinToString("")
+    var password = ""
 
-    val number = ("123456789").toList()
-    val randomNumber = (1..3).map { number.random() }.joinToString("")
+    for (i in 1..3) {
 
-    println("${randomChar[0]}${randomNumber[0]}${randomChar[1]}" +
-            "${randomNumber[1]}${randomChar[2]}${randomNumber[2]}")
+        val char = ('a'..'z').random()
+        val number = (0..9).random()
+
+        password = password + number + char
+    }
+    println(password)
 }
