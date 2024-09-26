2f79d20b3bb0b26ff28bb12caa8e2feb6e119911
Evgeniy <Evgrazhev@yandex.ru>
2024-09-26 09:16:05 +0300

    KS-18-2-dice-refactor.kt

--- src/main/kotlin/lesson_18/KS-18-2-dice.kt
+++ src/main/kotlin/lesson_18/KS-18-2-dice.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_18
 
 fun main() {
-    val cube = Dice(1..4)
+    val cube = Dice()
 
-    val cube2: Dice = Dice2(1..6)
+    val cube2: Dice = Dice2()
 
-    val cube3: Dice = Dice3(1..8)
+    val cube3: Dice = Dice3()
 
     val listCube: List<Dice> = listOf(cube, cube2, cube3)
 
@@ -14,18 +14,20 @@ fun main() {
     }
 }
 
-open class Dice(val edge: IntRange) {
+open class Dice {
+    open val edge = (1..4)
 
     open fun rollDice1(): Int = edge.random()
 }
 
-
-class Dice2(edge: IntRange) : Dice(edge) {
+class Dice2: Dice() {
+    override val edge = (1..6)
 
     override fun rollDice1(): Int = edge.random()
 }
 
-class Dice3(edge: IntRange) : Dice(edge) {
+class Dice3: Dice() {
+    override val edge = (1..8)
 
     override fun rollDice1(): Int = edge.random()
 }
