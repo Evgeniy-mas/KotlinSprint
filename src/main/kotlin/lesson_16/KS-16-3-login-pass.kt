6435826780f50539b13ec042717b641b505157f8
Evgeniy <Evgrazhev@yandex.ru>
2024-09-27 11:14:09 +0300

    KS-16-3-login-pass-refactor2.kt

--- src/main/kotlin/lesson_16/KS-16-3-login-pass.kt
+++ src/main/kotlin/lesson_16/KS-16-3-login-pass.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_16
 
 fun main() {
-    val user = UserData("Evgeniy", 1234)
+    val user = UserData("Evgeniy", "1234")
 
     println("Введите ваш пароль:")
-    val enterPassword = readln().toInt()
+    val enterPassword = readln()
 
     val result = user.checkPassword(enterPassword)
     println(result)
 }
 
-class UserData(val login: String, private val password: Int) {
+class UserData(val login: String, private val password: String) {
 
-    fun checkPassword(pass: Int): Boolean = pass == password
+    fun checkPassword(pass: String): Boolean = pass == password
 }
