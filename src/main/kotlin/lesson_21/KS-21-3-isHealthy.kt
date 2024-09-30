b7c33a6a6c3b2f9ac19152a44dc161577666d835
Evgeniy <Evgrazhev@yandex.ru>
2024-09-30 09:25:45 +0300

    KS-21-3-isHealthy-refactor.kt

--- src/main/kotlin/lesson_21/KS-21-3-isHealthy.kt
+++ src/main/kotlin/lesson_21/KS-21-3-isHealthy.kt
@@ -3,9 +3,9 @@ package org.example.lesson_21
 fun main() {
     val gamer = Gamer4("Gamer", 115, 200)
 
-    println(gamer.currentHealth.isHealthy())
+    println(gamer.isHealthy())
 }
 
 class Gamer4(val name: String, var currentHealth: Int, val maxHealth: Int)
 
-fun Int.isHealthy(): Boolean = this == 200
+fun Gamer4.isHealthy(): Boolean = this.currentHealth == this.maxHealth
