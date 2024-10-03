8cfba5af830d77754c89d0655e16cf66f5fa3610
Evgeniy <Evgrazhev@yandex.ru>
2024-10-03 14:51:48 +0300

    KS-16-4-order-refactor.kt

--- src/main/kotlin/lesson_16/KS-16-4-order.kt
+++ src/main/kotlin/lesson_16/KS-16-4-order.kt
@@ -3,17 +3,18 @@ package org.example.lesson_16
 fun main() {
     val pizza = Order(245, "в обработке")
 
-    newStatus(pizza, "готовится")
+    pizza.newStatus("готовится.")
 }
 
 class Order(private val number: Int, var status: String) {
 
-    fun changeStatus2(status: String) {
-        this.status = status
-        println("Статус заказа номер - $number, изменился на - ${this.status}.")
+    private fun changeStatus2(stat: String) {
+        status = stat
+        println("Статус заказа номер - $number, изменился на - $status.")
+    }
+
+    fun newStatus(status: String) {
+        changeStatus2(status)
     }
 }
 
-fun newStatus(order: Order, status: String) {
-    order.changeStatus2(status)
-}
