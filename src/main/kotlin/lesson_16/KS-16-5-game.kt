238f7e5e5ec226e1f00024e78d77dd48336a1985
Evgeniy <Evgrazhev@yandex.ru>
2024-10-08 09:20:24 +0300

    KS-16-5-game-refactor.kt

--- src/main/kotlin/lesson_16/KS-16-5-game.kt
+++ src/main/kotlin/lesson_16/KS-16-5-game.kt
@@ -1,5 +1,7 @@
 package org.example.lesson_16
 
+import kotlin.system.exitProcess
+
 fun main() {
     val gamer = Gamer5("Nick", 180, 25)
 
@@ -9,6 +11,7 @@ fun main() {
     gamer.damage(30)
     gamer.damage(40)
     gamer.damage(50)
+    gamer.damage(30)
 }
 
 class Gamer5(val name: String, var health: Int, var powerHit: Int) {
@@ -16,14 +19,14 @@ class Gamer5(val name: String, var health: Int, var powerHit: Int) {
     fun damage(damage: Int) {
         health -= damage
 
-        if (health in 1..49) {
-            healing()
-
-        } else if (health <= 0) {
+        if (health <= 0) {
             health = 0
             powerHit = 0
             println("Игрок $name умер, здоровье - $health, сила удара - $powerHit ")
-            return
+            exitProcess(0)
+
+        } else if (health < HEALTH_HEALING) {
+            healing()
         }
         println("Здоровье игрока $health")
     }
@@ -33,3 +36,5 @@ class Gamer5(val name: String, var health: Int, var powerHit: Int) {
         health += 20
     }
 }
+
+const val HEALTH_HEALING = 50
