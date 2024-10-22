c6ef783882b4afc1f20a25e948375faa9c626dd1
Evgeniy <Evgrazhev@yandex.ru>
2024-10-22 11:22:24 +0300

    KS-21-4-file-refactor.kt

--- src/main/kotlin/lesson_21/KS-21-4-file.kt
+++ src/main/kotlin/lesson_21/KS-21-4-file.kt
@@ -3,15 +3,16 @@ package org.example.lesson_21
 import java.io.File
 
 fun main() {
-    val file = File("How are you?")
-    file.writeNewText("Hello")
-    println(file)
+    val file = File("text")
+    file.createNewFile()
+
+    file.writeNewText("How are you?")
+    file.writeNewText("Hello!")
+    println(file.readText())
 }
 
 fun File.writeNewText(text: String) {
-    this.writeText(text)
-}
-
-fun File.lowercase(text: String) {
-
+    val newText = text.lowercase()
+    val thisText = this.readText()
+    this.writeText(newText + "\n" + thisText)
 }
